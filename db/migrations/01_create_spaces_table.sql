9c2c3bc6538b4fe9bdc1275e60b5cff74879ed68
john-baxter <john.g.baxter@googlemail.com>
2020-03-23 17:23:33 +0000

    add information about setting up the database - still incomplete, see README

---
+++ db/migrations/01_create_spaces_table.sql
@@ -0,0 +1,1 @@
+CREATE TABLE spaces(space_id SERIAL PRIMARY KEY, name VARCHAR(60), description VARCHAR(250), price SMALLINT NOT NULL, date_from DATE, date_to DATE);
---
+++ db/migrations/02_create_users_table.sql
@@ -0,0 +1,1 @@
+CREATE TABLE users (user_id SERIAL PRIMARY KEY, name VARCHAR(100), username VARCHAR(50), email VARCHAR(75), password VARCHAR(20)); 
