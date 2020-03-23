10f63bf2b96427dac20c25b54bab7545c7b21673
Anton Plisnier <anttongp@gmail.com>
2020-03-23 17:01:15 +0000

    #3

--- app.rb
+++ app.rb
@@ -4,7 +4,9 @@ class MakersBnB < Sinatra::Base
   get "/test" do
     "Testing infrastructure: self.code"
   end
-
+  get "/spaces" do
+    "42 Evergreen Terrace"
+  end
   run! if app_file == $0
 
 end
